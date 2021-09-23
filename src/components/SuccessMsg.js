5a076fa4e948a4dc22a312a19619c820f4951508
lisa <lisa.wang@cybersecthreat.com>
2021-09-23 18:07:38 +0800

    images compressed

--- src/components/SuccessMsg.js
+++ src/components/SuccessMsg.js
@@ -1,4 +1,5 @@
 import { AiOutlineCheckCircle } from "react-icons/ai";
+// import check_circle from "../../src/images/check_circle.gif";
 import "../styles/SuccessMsg.scss"
 
 function SuccessMsg(props) {
@@ -6,6 +7,7 @@ function SuccessMsg(props) {
         <div className="login-popup">
         <div className="overlay"></div>
             <form id="SuccessMsg" className="login-popup" onSubmit={props.onClick}>
+                {/* <img src={check_circle} alt="check_circle" /> */}
                 <AiOutlineCheckCircle/>
                 <h1>{props.t("success")}</h1>
                 <p>{props.message}</p>
